386de141163b53429dbf043cdd6897d0edc85289
amaciaszambrano <amacias@plannifai.com>
2023-08-23 21:57:34 +0200

    Add check property to item

--- lib/controllers/list_controller.dart
+++ lib/controllers/list_controller.dart
@@ -5,10 +5,10 @@ import '../models/item.dart';
 class ListController extends ChangeNotifier {
 
   List<Item> homeList = [
-    Item(0, 'Yogurt', 'Lacteo rico', 3),
-    Item(1, 'Chorizo', 'Lacteo rico', 2),
-    Item(2, 'Pan', 'Lacteo rico', 5),
-    Item(3, 'Croquetas', 'Lacteo rico', 10),
+    Item(0, 'Yogurt', 'Lacteo rico', 3, false),
+    Item(1, 'Chorizo', 'Chacina rica', 2, false),
+    Item(2, 'Pan', 'Trigo rico', 5, false),
+    Item(3, 'Croquetas', 'Frituria rica', 10, false),
   ];
   List<Item> shopList = [];
 
@@ -21,8 +21,7 @@ class ListController extends ChangeNotifier {
   void clearShopList() {
     homeList = List.from(homeList)..addAll(shopList);
     shopList.clear();
-    print(homeList);
-    print(shopList);
     notifyListeners();
   }
+
 }
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -4,22 +4,24 @@ class Item {
   late String _title;
   late String _description;
   late int _quantity;
+  late bool _check;
 
   int get id => _id;
   String get title => _title;
   String get description => _description;
   int get quantity => _quantity;
+  bool get check => _check;
 
-  set id(int value) => _id = value;
-  set title(String value) => _title = value;
-  set description(String value) => _description = value;
-  set quantity(int value) => _quantity = value;
-
-  Item(id, title, description, quantity) {
+  Item(id, title, description, quantity, check) {
     _id = id;
     _title = title;
     _description = description;
     _quantity = quantity;
+    _check = _check;
+  }
+
+  void setCheck(bool value) {
+    _check = value;
   }
 
 }
